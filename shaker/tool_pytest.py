f012996d5f42f576621804043ac27bd6546a26b7
Gabriela Leal <gabriela@vinta.com.br>
2022-08-08 22:03:56 -0300

    Setup environment before running test

--- shaker/tool_pytest.py
+++ shaker/tool_pytest.py
@@ -1,8 +1,7 @@
 from pathlib import Path
-import subprocess
 
 from base_tool import BaseTool
-from util import subprocess_run
+from util import subprocess_run, run_bash_command_with_output
 
 
 class Pytest(BaseTool):
@@ -11,12 +10,12 @@ class Pytest(BaseTool):
         create_env_command = f"python3 -m venv {env_name}"
         subprocess_run(create_env_command, cwd=str(self.directory))
 
-        activate_env_command = f"sudo source {env_name}/bin/activate"
-        subprocess_run(activate_env_command, cwd=str(self.directory))
+        activate_env_command = f"source {env_name}/bin/activate"
+        run_bash_command_with_output(activate_env_command, cwd=str(self.directory), shell=True)
 
     def _deactivate_virtual_environment(self):
         activate_env_command = "deactivate"
-        subprocess_run(activate_env_command, cwd=str(self.directory))
+        run_bash_command_with_output(activate_env_command, cwd=str(self.directory), shell=True)
 
     def setup(self):
         self._create_virtual_environment()
@@ -29,8 +28,16 @@ class Pytest(BaseTool):
             subprocess_run(command, cwd=str(requirements_file.parent))
         else:
             print(f"> Requirements file not found. Installing package...")
-            command = f"pip install git+{self.project_url}"
-            subprocess_run(command, cwd=str(requirements_file.parent))
+
+            # Install packages that were breaking the tests
+            install_pytest = "pip3 install pytest"
+            run_bash_command_with_output(install_pytest, cwd=str(self.directory), shell=True)
+
+            install_pytest = "pip3 install hypothesis"
+            run_bash_command_with_output(install_pytest, cwd=str(self.directory), shell=True)
+
+            command = "pip3 install git+%(url)s" % {"url": self.project_url}
+            run_bash_command_with_output(command, cwd=str(requirements_file.parent), shell=True)
 
     def tear_down(self):
         self._deactivate_virtual_environment()
@@ -44,4 +51,4 @@ class Pytest(BaseTool):
         
         # pytest Test_filename::Test_classname::Test_funcname --junitxml {}
         print(f"> {command}")
-        subprocess_run(command, cwd=str(self.directory))
+        run_bash_command_with_output(command, cwd=str(self.directory), shell=True)
