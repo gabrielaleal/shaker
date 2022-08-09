2f6435fe49ce97b7e089c69a4f85eeb7a55468eb
Gabriela Leal <gabriela@vinta.com.br>
2022-08-08 22:00:56 -0300

    Add helper that runs shell commands

--- shaker/util.py
+++ shaker/util.py
@@ -1,11 +1,18 @@
 from shlex import split
-from subprocess import DEVNULL, Popen, run
+from subprocess import DEVNULL, PIPE, Popen, run
+
+
+def run_bash_command_with_output(command, cwd=None, shell=False):
+    """
+    With this helper, if any errors are raised while running the commands,
+    they will be displayed on the terminal.
+    """
+
+    Popen(args=command, stdout=PIPE, cwd=cwd, shell=shell)
 
 
 def subprocess_run(command, stdout=DEVNULL, stderr=DEVNULL, cwd=None):
     args = split(command)
-
-    # TODO: add output to debug
 
     return run(args, stdout=stdout, stderr=stderr, cwd=cwd)
 
