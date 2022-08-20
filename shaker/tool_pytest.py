0121572c9290bb168ad70b9948a5017b549d63d3
Gabriela Leal <gabriela@vinta.com.br>
2022-08-20 10:44:18 -0300

    Update virtual environment activation/deactivation

--- shaker/tool_pytest.py
+++ shaker/tool_pytest.py
@@ -7,37 +7,37 @@ from util import subprocess_run, run_bash_command_with_output
 class Pytest(BaseTool):
     def _create_virtual_environment(self):
         env_name = "env"
-        create_env_command = f"python3 -m venv {env_name}"
+        create_env_command = f"python -m venv {env_name}"
         subprocess_run(create_env_command, cwd=str(self.directory))
 
-        activate_env_command = f"source {env_name}/bin/activate"
+        print("Activating virtual environment...")
+
+        # activate_env_command = f"source {env_name}/bin/activate"
+        activate_env_command = f". {env_name}/bin/activate"
         run_bash_command_with_output(activate_env_command, cwd=str(self.directory), shell=True)
 
     def _deactivate_virtual_environment(self):
-        activate_env_command = "deactivate"
+        print("Deactivating virtual environment...")
+        activate_env_command = ". deactivate"
         run_bash_command_with_output(activate_env_command, cwd=str(self.directory), shell=True)
 
     def setup(self):
         self._create_virtual_environment()
-
         requirements_file = Path(self.directory / "requirements.txt")
+
+        # if pipfile.exists():
+        #     # Install all dependencies for a project (including dev ones)
+        #     command = "pipenv install --dev"
+        #     # By installing, pipenv creates a virtual environment and installs all dependencies
+        #     run_bash_command_with_output(command, cwd=str(pipfile.parent), shell=True)
+
+        install_pytest = "pip install pytest"
+        run_bash_command_with_output(install_pytest, cwd=str(self.directory), shell=True)
 
         if requirements_file.exists():
             print(f"> Installing dependencies from requirements.txt")
             command = "pip install -r requirements.txt"
             subprocess_run(command, cwd=str(requirements_file.parent))
-        else:
-            print(f"> Requirements file not found. Installing package...")
-
-            # Install packages that were breaking the tests
-            install_pytest = "pip3 install pytest"
-            run_bash_command_with_output(install_pytest, cwd=str(self.directory), shell=True)
-
-            install_pytest = "pip3 install hypothesis"
-            run_bash_command_with_output(install_pytest, cwd=str(self.directory), shell=True)
-
-            command = "pip3 install git+%(url)s" % {"url": self.project_url}
-            run_bash_command_with_output(command, cwd=str(requirements_file.parent), shell=True)
 
     def tear_down(self):
         self._deactivate_virtual_environment()
